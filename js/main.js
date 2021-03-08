22f529723c4cd7737ec21fcbea80202dde46231f
kpaczek <cloudie313@gmail.com>
2021-03-08 18:14:47 +0100

    number testing

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let n = 1154;
+
+const numberBeetwen = a => {
+    if (a >= 100 & a <= 200) {
+        console.log('Liczba znajduje się w przedziale');
+    } else {
+        console.log('Liczba nie znajduje się w przedziale');
+    }
+}
+
+numberBeetwen(n);
